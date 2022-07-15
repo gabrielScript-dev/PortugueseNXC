d0ced5df8f30cb9851710e124c3e3058df854587
gabrielScript-dev <gabriel.script@gmail.com>
2022-07-15 01:28:07 -0300

    Update PortugueseNXC.h
    Modificações de aprimoramento e adição de novas funcionalidades para.

--- PortugueseNXC.h
+++ PortugueseNXC.h
@@ -3,11 +3,11 @@
 
 //Tarefa Principal
 #define tarefa task
-#define principal main
+#define principal main()
 
 //Argumentos Lógicos
-#define verdadeiro true
-#define falso true
+#define verdadeiro 1
+#define falso 0
 
 //Constantes
 #define PORTA_1 IN_1
@@ -20,16 +20,21 @@
 #define PORTA_AB OUT_AB
 #define PORTA_AC OUT_AC
 #define PORTA_BC OUT_BC
-#define LINHA1 LCD_LINE1
-#define LINHA2 LCD_LINE2
-#define LINHA3 LCD_LINE3
-#define LINHA4 LCD_LINE4
+#define LINHA_1 LCD_LINE1
+#define LINHA_2 LCD_LINE2
+#define LINHA_3 LCD_LINE3
+#define LINHA_4 LCD_LINE4
+#define LINHA_5 LCD_LINE5
+#define LINHA_6 LCD_LINE6
+#define LINHA_7 LCD_LINE7
+#define LINHA_8 LCD_LINE8
 
 //Tipos de Variáveis
 #define funcao void
 #define inteiro int
 #define logico bool
 #define decimal float
+#define cadeia string
 #define caractere char
 #define constante const
 
@@ -44,7 +49,7 @@
 #define fazer do	
 #define para for
 #define sair break
-#define repita(x) for(int counter = 0; counter < x; counter++);
+#define repita(x) for(int counter = 0; counter < x; counter++)
 #define loop while(true)
 
 //Operadores Relacionais
@@ -59,14 +64,29 @@
 #define e &&
 #define ou ||
 
+
+//Potênciação e Radiciação
+
+#define Raiz(num) Sqrt(num);
+
+float Potencia(float base, float exponent) {
+	float result = 1;
+	for(exponent; exponent > 0; exponent--) {
+		result = result * base;
+	}
+
+	return result; 
+}
+
 //---------------Comandos---------------------
 
 //Motor
 #define Parar(port) Off(port);
 #define PararSuave(port) Float(port);
 #define ParaTras(port, pwr) OnRev(port, pwr);
+#define ParaTrasEx(port, pwr, turnpct) OnRevSync(port, pwr, turnpct);
 #define ParaFrente(port, pwr) OnFwd(port, pwr);
-#define ParaFrenteP(port, pwr, proportional) OnFwdSync(port, pwr, proportional);
+#define ParaFrenteEx(port, pwr, turnpct) OnFwdSync(port, pwr, turnpct);
 #define GirarMotor(port, pwr, degrees) RotateMotor(port, pwr, degrees);
 #define GirarMotorEx(port, pwr, degrees, turnpct) RotateMotorEx(port, pwr, degrees, turnpct, true, true);
 
@@ -79,6 +99,18 @@
 #define ImprimirNum(x, y, num) NumOut(x, y, num);
 #define ImprimirTexto(x, y, text) TextOut(x, y, text);
 #define ImprimirImg(x, y, file_name) GraphicOut(x, y, file_name);
+
+
+//String
+//convertendo variável lógica para uma String
+string LogicoParaCadeia(bool logic) {
+	if(logic)
+		return "verdadeiro";
+	else
+		return "falso";
+}
+#define NumParaCadeia(num) NumToStr(num);
+#define TamanhoDaCadeia(str) StrLen(str);
 
 //Sensor
 #define SensorDeLuz(port) SetSensorLight(port);
